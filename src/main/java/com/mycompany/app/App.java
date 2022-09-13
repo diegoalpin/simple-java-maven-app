fea105a62e8bcd3993e9d4adb76293f72693ef0c
Diego Alpin <d1349611@ncsdevnet.com>
2022-09-13 15:37:32 +0800

    commit another

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Guys it's training Time!";
+    private final String message = "Hello Guys it's training Time again!!";
 
     public App() {}
 
